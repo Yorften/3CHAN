0210de0fc790e4b916349f7f853a873568b6402d
Abderrahman Badi <144786380+Yorften@users.noreply.github.com>
2024-10-09 01:09:34 +0100

    CH-20 CommentService tests done

--- src/test/java/service/CommentServiceTest.java
+++ src/test/java/service/CommentServiceTest.java
@@ -5,6 +5,8 @@ import static org.junit.Assert.assertTrue;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Optional;
 
 import org.junit.Before;
@@ -35,12 +37,25 @@ public class CommentServiceTest {
     private CommentService commentService;
 
     private Comment comment;
+    private List<Comment> commentsList;
 
     @Before
     public void setUp() {
         comment = new Comment();
         comment.setId(1L);
         comment.setContent("This is a test comment");
+
+        commentsList = new ArrayList<>();
+        Comment comment1 = new Comment();
+        comment1.setId(2L);
+        comment1.setContent("Second test comment");
+
+        Comment comment2 = new Comment();
+        comment2.setId(3L);
+        comment2.setContent("Third test comment");
+
+        commentsList.add(comment1);
+        commentsList.add(comment2);
 
         commentService = new CommentService(commentRepository);
     }
@@ -54,12 +69,59 @@ public class CommentServiceTest {
 
         Optional<Comment> result = commentService.getComment(1L);
 
-        System.out.println(result);
-
         assertTrue(result.isPresent());
         assertEquals("This is a test comment", result.get().getContent());
 
         verify(commentRepository).get(1L);
+    }
+
+    @Test
+    public void getAllComentsTest() {
+        when(commentRepository.getAll(1L)).thenReturn(commentsList);
+
+        List<Comment> result = commentService.getAllComments(1L);
+
+        assertEquals(2, result.size());
+        assertEquals("Second test comment", result.get(0).getContent());
+        assertEquals("Third test comment", result.get(1).getContent());
+
+        verify(commentRepository).getAll(1L);
+    }
+
+    @Test
+    public void saveCommentTest() {
+
+        Comment commentToSave = new Comment();
+        commentToSave.setId(1L);
+        commentToSave.setContent("This is a test comment to save");
+
+        commentService.saveComment(commentToSave);
+
+        verify(commentRepository).save(commentToSave);
+    }
+
+    @Test
+    public void updateCommentTest() {
+
+        Comment commentToUpdate = new Comment();
+        commentToUpdate.setId(1L);
+        commentToUpdate.setContent("This is a test comment to update");
+
+        commentService.updateComment(commentToUpdate);
+
+        verify(commentRepository).update(commentToUpdate);
+    }
+
+    @Test
+    public void deleteCommentTest() {
+
+        Comment commentToDelete = new Comment();
+        commentToDelete.setId(1L);
+        commentToDelete.setContent("This is a test comment to delete");
+
+        commentService.deleteComment(commentToDelete.getId());
+
+        verify(commentRepository).delete(commentToDelete.getId());
     }
 
 }
