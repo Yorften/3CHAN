d248552bfc74b46185df4bc92a58679948a68a3d
Ayoub CHAABAT <chaabat.02@gmail.com>
2024-10-09 10:12:51 +0100

    CH-23 Create Article Interface

--- src/main/java/repository/interfaces/ArticleRepository.java
+++ src/main/java/repository/interfaces/ArticleRepository.java
@@ -1,5 +1,25 @@
 package repository.interfaces;
 
+import java.util.List;
+
+import model.Article;
+
 public interface ArticleRepository {
+	
+	List<Article> getAllArticles(int page , int pageSize);
+	
+	Article getArticleById(long id);
+	
+	List<Article> searchArticleByTitle(String title ,int page , int pageSize);
+	
+	void addArticle(Article article);
+	
+	void updateArticle(Article article);
+	
+	void deleteArticle(Long id);
+	
+	long getTotalArticleCount();
+	
+	
 
 }
