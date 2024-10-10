af8675d5eddafddd916447e38cb630cb127a6e40
Walid Saifi <saifiwalid21@gmail.com>
2024-10-10 10:07:22 +0100

    CH-24  ajouter add author a Repository

--- src/main/java/repository/implementation/AuthorRepositoryImpl.java
+++ src/main/java/repository/implementation/AuthorRepositoryImpl.java
@@ -1,5 +1,37 @@
 package repository.implementation;
 
-public class AuthorRepositoryImpl {
+import model.Author;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import repository.interfaces.AuthorRepository;
+import util.PersistenceUtil;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityTransaction;
+
+public class AuthorRepositoryImpl implements AuthorRepository {
+
+    private static final Logger logger = LoggerFactory.getLogger(AuthorRepositoryImpl.class);
+    
+
+    @Override
+    public void addAuthor(Author author) {
+        EntityManager entityManager = PersistenceUtil.getEntityManagerFactory().createEntityManager();
+        EntityTransaction transaction = null;
+        try {
+            transaction = entityManager.getTransaction();
+            transaction.begin();
+            entityManager.persist(author);
+            transaction.commit();
+        } catch (Exception e) {
+            if (transaction != null && transaction.isActive()) {
+                transaction.rollback();
+            }
+            logger.error("Error Adding Author", e);
+        } finally {
+            entityManager.close();
+        }
+    }
+
 
 }
--- src/main/java/repository/interfaces/AuthorRepository.java
+++ src/main/java/repository/interfaces/AuthorRepository.java
@@ -1,5 +1,8 @@
 package repository.interfaces;
 
+import model.Author;
+
 public interface AuthorRepository {
+    public void addAuthor(Author author);
 
 }
