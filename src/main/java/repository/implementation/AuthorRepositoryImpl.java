a6d2e0355b406955a32996bd07c0f0c3d9ceca29
Walid Saifi <saifiwalid21@gmail.com>
2024-10-10 10:14:40 +0100

    CH-24  ajouter ----- authorRepository

--- src/main/java/repository/implementation/AuthorRepositoryImpl.java
+++ src/main/java/repository/implementation/AuthorRepositoryImpl.java
@@ -8,6 +8,7 @@ import util.PersistenceUtil;
 
 import javax.persistence.EntityManager;
 import javax.persistence.EntityTransaction;
+import java.util.List;
 
 public class AuthorRepositoryImpl implements AuthorRepository {
 
@@ -51,6 +52,42 @@ public class AuthorRepositoryImpl implements AuthorRepository {
         } finally {
             entityManager.close();
         }
+    }
+
+    @Override
+    public void deleteAuthor(Long authorId) {
+        EntityManager entityManager = PersistenceUtil.getEntityManagerFactory().createEntityManager();
+        EntityTransaction transaction = null;
+        try {
+            transaction = entityManager.getTransaction();
+            transaction.begin();
+            Author author = entityManager.find(Author.class, authorId);
+            if (author != null) {
+                entityManager.remove(author);
+            }
+            transaction.commit();
+        } catch (Exception e) {
+            if (transaction != null && transaction.isActive()) {
+                transaction.rollback();
+            }
+            logger.error("Error Deleting Author", e);
+        } finally {
+            entityManager.close();
+        }
+    }
+
+    @Override
+    public List<Author> getAllAuthors() {
+        EntityManager entityManager = PersistenceUtil.getEntityManagerFactory().createEntityManager();
+        List<Author> authors = null;
+        try {
+            authors = entityManager.createQuery("SELECT a FROM Author a", Author.class).getResultList();
+        } catch (Exception e) {
+            logger.error("Error Retrieving Authors", e);
+        } finally {
+            entityManager.close();
+        }
+        return authors;
     }
 
 
--- src/main/java/repository/interfaces/AuthorRepository.java
+++ src/main/java/repository/interfaces/AuthorRepository.java
@@ -2,8 +2,12 @@ package repository.interfaces;
 
 import model.Author;
 
+import java.util.List;
+
 public interface AuthorRepository {
     public void addAuthor(Author author);
     public void updateAuthor(Author author);
+    public void deleteAuthor(Long authorId);
+    public List<Author> getAllAuthors();
 
 }
